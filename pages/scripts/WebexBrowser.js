d1850e1857c81f6e36083774faa0dc97c5d67554
Simon <cactushamsters@gmail.com>
2021-03-17 01:18:02 -0700

    KAOMOJIS

--- pages/scripts/WebexBrowser.js
+++ pages/scripts/WebexBrowser.js
@@ -1,25 +1,26 @@
 var webex
-
+var x = 0
 var New_Pineapple_Spaces = 'Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNjI1ZGQ1MDAtNzQ1OS0xMWViLWJkZTMtYTU3MGVlOTk0YTll';
-
+var RoomID
+var x = 0
 
 
 
 function initWebex(token) {
-var webex = window.Webex.init({
+webex = window.Webex.init({
   credentials: {
     access_token: token}
 });
 }
 
-
-
-
+//Use Pineapple by default on load.
+initWebex('NGQ0NTU3ZjgtODhmMi00OTdjLWE5Y2QtNzFkZmNlMTJkYzg0N2Y5ZWQzYjEtMDUx_PF84_011f8b79-70e7-45af-852e-e7e46203c1ff');
+deleteRoomList();
+MakeRoomList();
 
 //Token button
 authChange.onclick = function (e) {
-try{
-tokenIsSet()
+//try{
 
 
 initWebex(tokenBox.value);
@@ -31,32 +32,124 @@ document.getElementById("change").innerHTML = '-------------------------'
 document.getElementById("change").style = 'color:#000000'
 }, 3000);
 //console.log(tokenBox.value);
-} catch (e) {
+/*} catch (e) {
 document.getElementById("change").innerHTML = 'Authtoken not set. No access point, perhaps?'
 document.getElementById("change").style = 'color:#cc0000'
 setTimeout(function(){ 
 document.getElementById("change").innerHTML = '-------------------------'
 document.getElementById("change").style = 'color:#000000'	
 }, 3000); //End of timeout
-};//End of catch
+};//End of catch*/
+tokenIsSet();
 } //End of function
 
 
 
 
 
-function tokenIsSet() {
-document.getElementById("change").visibility = ''
 
-	
-	
+
+//Token button
+PineappleAuth.onclick = function (e) {
+//try{
+initWebex('NGQ0NTU3ZjgtODhmMi00OTdjLWE5Y2QtNzFkZmNlMTJkYzg0N2Y5ZWQzYjEtMDUx_PF84_011f8b79-70e7-45af-852e-e7e46203c1ff');
+document.getElementById("change").innerHTML = 'Authtoken changed!'
+document.getElementById("change").style = 'color:#1d8212'
+
+setTimeout(function(){ 
+document.getElementById("change").innerHTML = '-------------------------'
+document.getElementById("change").style = 'color:#000000'
+}, 3000);
+//console.log(tokenBox.value);
+/*} catch (e) {
+document.getElementById("change").innerHTML = 'Authtoken not set. No access point, perhaps?'
+document.getElementById("change").style = 'color:#cc0000'
+setTimeout(function(){ 
+document.getElementById("change").innerHTML = '-------------------------'
+document.getElementById("change").style = 'color:#000000'
+
+document.getElementById("tokenBox").value = 'NGQ0NTU3ZjgtODhmMi00OTdjLWE5Y2QtNzFkZmNlMTJkYzg0N2Y5ZWQzYjEtMDUx_PF84_011f8b79-70e7-45af-852e-e7e46203c1ff'
+
+}, 3000); //End of timeout
+};//End of catch*/
+tokenIsSet();
+} //End of function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function tokenIsSet() {
+deleteRoomList();
+MakeRoomList();
+//document.createElement(tagName[, options]);
+}
+
+function deleteRoomList() {
+/*var z = document.getElementsByClassName("roomSelectScript"); for (var i = 0; i < z.length; i++) { z[i].remove();}; var z = document.getElementsByClassName("roomSelectList"); for (var i = 0; i < z.length; i++) {z[i].remove();}*/
+for (var i=0;i<10;i++) {
+for (const i of document.getElementsByClassName("roomSelectList")) {console.log('hi'); i.remove();
+}//End of smoller for
+}//End of big for
+}//End of function
+
+function MakeRoomList() {
+webex.rooms.list({ //List rooms with SDK
+max: 50
+})
+.then((rooms) => {
+console.log('Listing rooms:')
+      for (const room of rooms.items) {
+        let roomName = room.title
+		let roomid = room.id
+	  console.log(`${roomName}`);
+x = x+1
+//Add a generated piece to the dropdown
+var piece = document.createElement("A");
+piece.innerHTML = roomName;
+piece.className = 'roomSelectList';
+piece.id = 'roomSelectList' + x;
+document.getElementById("RoomSelectDiv").appendChild(piece);
+//Add a generated script for each room choice
+var script = document.createElement("SCRIPT");
+script.innerHTML = 'roomSelectList'+ x +'.onclick = function (e) {console.log("' + roomid + '"); RoomID ="' + roomid + '";}'; 
+script.className = 'roomSelectScript';
+var a = "roomSelectList" + x
+document.getElementById(a).appendChild(script);
+	  } //End of for
+console.log('Done listing rooms.')
+    }) //End of .then
 }
 
 
+var update = setInterval(Update, 5000);
+function Update() {
+
+
+
+}
 
 
 function say(message, roomid = New_Pineapple_Spaces) {
-	
+
 }
 
 
---
+++ pages/scripts/kaomoji.js
@@ -0,0 +1,88 @@
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+var prebuilt = ['ヾ(•ω•`)o','o((>ω< ))o','☜(ﾟヮﾟ☜)']
+
+document.getElementById("titleKao").innerHTML = prebuilt[random(1,2)-1]
+
+
+
+
+
+var enhance = false
+function EnhanceRandomize () {
+if (enhance == true) {
+	enhance = false;
+	document.getElementById("enhancer").style = 'background-color: darkslategrey; color:white; padding: 12px; font-size: 18px; border-radius: 10px; border: none;';
+} else {
+	enhance = true;
+	document.getElementById("enhancer").style = 'background-color: indianred; color:white; padding: 12px; font-size: 18px; border-radius: 10px; border: none;';	
+}	
+console.log(enhance)
+}
+//enhancer.onclick = function (e) {EnhanceRandomize()}
+
+
+
+
+function generate() {
+if (enhance == false) {
+var kaomoji = ''
+var leftArm = ['╰','ヽ','੧','⋋','ლ','ლ','ᕙ','୧','┌','└','٩','ᕦ','へ','¯\\_','╚','═','c','乁','o͡͡͡╮','/╲/\╭','凸','〜','┌∩┐','∩','╭∩╮',''] //26
+var rightArm = ['凸','〜','┌∩┐','∩','╭∩╮','╯','ﾉ','ノ','੭','⋌','⊃','つ','ᕗ','୨','┐','┘','و','ง','ᕤ','ᓄ','_/¯','═╝','ㄏ','ᕤ','╭','o͡͡͡','ノ','⌒','.',"╮/\╱﻿\\",''] //30
+var cheek = ['.','✿','˵','v','*','”','=','~','∗',':',' '] //11
+var eye = ['^','＾','o','°','•́','•̀','￣','ݓ','✖','･','・','﹒','՞','︣','⌣','@',' '] //17
+var mouth = ['ω','_','ਊ','︿','o','〜','〰','∧','Д','д','۝','ڡ','ʖ','▽','∀',' '] //16
+var leftHead = ['(','[','༼','ʕ','໒','(','(','(','('] //9
+var rightHead = [')',']','༽','ʔ',')','७',')',')',')'] //9
+kaomoji = kaomoji + leftArm[random(1,26)-1]
+kaomoji = kaomoji + leftHead[random(1,9)-1]
+kaomoji = kaomoji + cheek[random(1,11)-1]
+kaomoji = kaomoji + eye[random(1,17)-1]
+kaomoji = kaomoji + mouth[random(1,16)-1]
+kaomoji = kaomoji + eye[random(1,17)-1]
+kaomoji = kaomoji + cheek[random(1,11)-1]
+kaomoji = kaomoji + rightHead[random(1,9)-1]
+kaomoji = kaomoji + rightArm[random(1,30)-1]
+document.getElementById("kaomoji").innerHTML = kaomoji;
+} else {
+var kaomoji = ''
+var leftArm = ['╰','ヽ','੧','⋋','ლ','ლ','ᕙ','୧','┌','└','٩','ᕦ','へ','¯\\_','╚','═','c','乁','凸','〜','┌∩┐',''] //22
+var rightArm = ['凸','〜','┌∩┐','╯','ﾉ','ノ','੭','⋌','⊃','つ','ᕗ','୨','┐','┘','و','ง','ᕤ','ᓄ','═╝','ㄏ','ᕤ','╭','ノ','⌒','.',''] //26
+var cheek = ['.','✿','˵','v','*','”','=','~','∗',':',' ',' ',' ',' ',' ',' '] //16
+var eye = ['^','＾','o','°','•́','•̀','￣','ݓ','･','・','﹒','՞','︣','@',' '] //15
+var mouth = ['ω','_','ਊ','︿','o','〜','〰','∧','Д','д','ڡ','ʖ','▽','∀','0'] //15
+var Head1 = ['(','[','('] //3
+var Head2 = [')',']',')'] //3
+var head = random(1,3)-1
+var EYE = random(1,15)-1
+
+kaomoji = kaomoji + leftArm[random(1,22)-1]
+kaomoji = kaomoji + Head1[head]
+kaomoji = kaomoji + cheek[random(1,12)-1]
+kaomoji = kaomoji + eye[EYE]
+kaomoji = kaomoji + mouth[random(1,15)-1]
+if (eye[EYE] == '￣' | eye[EYE] == '^' | eye[EYE] == 'ݓ' | eye[EYE] == '﹒' | eye[EYE] == 'ݓ') {kaomoji = kaomoji + eye[EYE]} else {kaomoji = kaomoji + eye[EYE-(random(1,2)-1)]}
+kaomoji = kaomoji + cheek[random(1,11)-1]
+kaomoji = kaomoji + Head2[head]
+if (leftArm == '¯\\_') {kaomoji = kaomoji + '_/¯'} else {kaomoji = kaomoji + rightArm[random(1,26)-1]}
+document.getElementById("kaomoji").innerHTML = kaomoji;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
